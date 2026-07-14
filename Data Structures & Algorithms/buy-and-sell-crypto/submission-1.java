10af7ec78810bee4d7d2de88871b63a9cbd7982c
Hari Prasad <hariprasadacharya993@gmail.com>
2026-07-14 15:11:36 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int maxprofit=0;
+        int minprice=prices[0];
+
+        for(int i=1;i<=prices.length-1;i++){
+            if(prices[i]<minprice){
+                minprice=prices[i];
+            }
+            else{
+                int profit=prices[i]-minprice;
+
+                if(profit>maxprofit){
+                    maxprofit=profit;
+                }
+
+            }
+        }
+        return maxprofit;
+        
+    }
+}
